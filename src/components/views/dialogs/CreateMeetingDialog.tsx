1a6d546f71ad0264c9e3d85703aa09d0c2e66bf4
MaHa-Nordeck <68593282+MaHa-Nordeck@users.noreply.github.com>
2020-09-22 14:21:41 +0200

    Created most of the MeetingDialog and user selection component
    *CreateMeetingDialog.tsx - created > Modal that allows for the creation of meetings
    *UserSelection.js - created > User list component.

--- src/components/views/dialogs/CreateMeetingDialog.tsx
+++ src/components/views/dialogs/CreateMeetingDialog.tsx
@@ -1,20 +1,112 @@
-
 import React from 'react';
+import {_t} from '../../../languageHandler';
 import BaseDialog from '../dialogs/BaseDialog';
-import { _t } from '../../../languageHandler';
+import DialogButtons from '../elements/DialogButtons';
+import Field from '../elements/Field';
+import Dropdown from '../elements/Dropdown';
+import {MatrixClientPeg} from "../../../MatrixClientPeg";
+import DMRoomMap from '../../../utils/DMRoomMap'
+import RoomMember from "matrix-js-sdk/src/models/room-member";
+import RoomViewStore from '../../../stores/RoomViewStore';
+import UserSelection from "../rooms/UserSelection";
 
-interface IProps {
-    defaultPublic?: boolean;
-    onFinished(): void;
+interface IState {
+    meetingTopic: string;
+    meetingDate: string;
+    meetingTimeFrom: string;
+    meetingTimeTill: string;
+    parentRoom: string;
+    userSelection: Record<string, RoomMember>;
 }
 
-export default class CreateMeetingDialog extends React.Component<IProps> {
-    public render() {
-        const title = _t("Schedule Meeting");
-        return(
-            // replace className with mx_CreateMeetingDialog
-            <BaseDialog className="mx_CreateGroupDialog" onFinished={this.props.onFinished} title={title}>
+export default class CreateMeetingDialog extends React.Component<IState> {
 
+// Util functions
+    private formattedDate = (d: Date = new Date()): string => {
+        return d.toISOString().split('T')[0];
+    }
+
+    private formattedTime = (d: Date = new Date()): string => {
+        return new Intl.DateTimeFormat(undefined,{hour: '2-digit', minute: '2-digit', hour12: false}).format(d);
+    }
+
+    private germanDate = (d: Date = new Date()): string => {
+        return d.toLocaleDateString("de-DE", { day: '2-digit', month: '2-digit', year: 'numeric'});
+    }
+
+    private generateMeetingTitle = (roomName: string): string => {
+        const date = new Date(this.state.meetingDate);
+        return roomName + "-" + this.germanDate(date) + "-" + this.state.meetingTimeFrom;
+    }
+
+    private getRoomMembers = (id: string) => {
+        return MatrixClientPeg.get().getRoom(id).currentState.members;
+    }
+
+    private isDM = (room_id: string): boolean => {
+        const id = DMRoomMap.shared().getUserIdForRoomId(room_id);
+        return id !== undefined && id !== "";
+    }
+
+    private setUserSelection = (selection) => {
+        this.setState({userSelection: selection});
+    }
+
+// modal interactivity functions
+    private onOk = () => {return null;}
+    private onFinished = () => {return null;}
+    private onCancel = () => {return null;}
+    private onParentRoomChange = (ev) => {
+        this.setState({parentRoom: ev});
+    }
+    private onUserSelectChange = () => {return null;}
+    private onTopicChange = (ev) => {this.setState({meetingTopic: ev.target.value})}
+    private onDateChange = (ev) => {this.setState({meetingDate: ev.target.value})}
+    private onFromChange = (ev) => {this.setState({meetingTimeFrom: ev.target.value})}
+    private onTillChange = (ev) => {this.setState({meetingTimeTill: ev.target.value})}
+
+    state = {
+        meetingTopic: "",
+        meetingDate: this.formattedDate(),
+        meetingTimeFrom: this.formattedTime(new Date()),
+        meetingTimeTill: this.formattedTime(new Date(Date.now() + (60*60*1000))),
+        parentRoom: RoomViewStore.getRoomId(),
+        userSelection: {}
+    };
+
+    public render() {
+        const client = MatrixClientPeg.get();
+        const rooms = client.getVisibleRooms().filter(room => !this.isDM(room.roomId));
+        const currentRoom = client.getVisibleRooms().filter(room => room.roomId === this.state.parentRoom)[0];
+        const meetingTitle = this.generateMeetingTitle(currentRoom.name);
+        const parentRoomOptions = rooms.map(room => {
+            return <div key={ room.roomId }>{ room.name }</div>
+        });
+
+        return(
+            <BaseDialog className="mx_CreateMeetingDialog" title={_t("Schedule Meeting")} onFinished={this.onFinished} >
+                <form onSubmit={this.onOk}>
+                    <Field type="text" value={ meetingTitle } label={_t("Meeting Title")} disabled/>
+                    <Field type="text" value={this.state.meetingTopic} onChange={this.onTopicChange} label={_t("Meeting Topic")}/>
+                    <Dropdown
+                        id="parentRoomDropDown"
+                        label={_t("Parent Room")}
+                        value={this.state.parentRoom}
+                        onOptionChange={this.onParentRoomChange}
+                        className="mx_Dropdown"
+                    >
+                        { parentRoomOptions }
+                    </Dropdown>
+                    <Field type="date" value={this.state.meetingDate} label={_t("Date")} onChange={this.onDateChange} />
+                    <Field type="time" value={this.state.meetingTimeFrom} label={_t("From")} onChange={this.onFromChange} />
+                    <Field type="time" value={this.state.meetingTimeTill} label={_t("Till")} onChange={this.onTillChange} />
+                    <DialogButtons
+                        primaryButton={_t('Create Meeting')}
+                        onPrimaryButtonClick={this.onOk}
+                        onCancel={this.onCancel}
+                    />
+                    <UserSelection roomId={this.state.parentRoom} userSelectionCallback={this.setUserSelection} />
+                </form>
             </BaseDialog>
         );
     }
---
+++ src/components/views/rooms/UserSelection.js
@@ -0,0 +1,374 @@
+import React, {createRef} from 'react';
+import PropTypes from 'prop-types';
+import {_t} from "../../../languageHandler";
+import {MatrixClientPeg} from "../../../MatrixClientPeg";
+import {getHttpUriForMxc} from "matrix-js-sdk/src/content-repo";
+import {humanizeTime} from "../../../utils/humanize";
+import BaseAvatar from "../../views/avatars/BaseAvatar";
+import AccessibleButton from "../../views/elements/AccessibleButton";
+import {makeUserPermalink} from "../../../utils/permalinks/Permalinks";
+import {RoomMember} from "matrix-js-sdk/src/matrix";
+import * as sdk from "../../../index";
+import {getDefaultIdentityServerUrl, useDefaultIdentityServer} from "../../../utils/IdentityServerUtils";
+import {abbreviateUrl} from "../../../utils/UrlUtils";
+import dis from "../../../dispatcher/dispatcher";
+import {Action} from "../../../dispatcher/actions";
+import RoomViewStore from "../../../stores/RoomViewStore";
+
+const INITIAL_USERS_SHOWN = 10;
+const INCREMENT_USER_SHOWN = 10;
+
+class Member {
+    get name(): string { throw new Error("Member class not implemented"); }
+    get userId(): string { throw new Error("Member class not implemented"); }
+    getMxcAvatarUrl(): string { throw new Error("Member class not implemented"); }
+}
+
+class DMRoomTile extends React.PureComponent {
+    static propTypes = {
+        member: PropTypes.object.isRequired, // Should be a Member (see interface above)
+        lastActiveTs: PropTypes.number,
+        onToggle: PropTypes.func.isRequired, // takes 1 argument, the member being toggled
+        highlightWord: PropTypes.string,
+        isSelected: PropTypes.bool,
+    };
+
+    _onClick = (e) => {
+        // Stop the browser from highlighting text
+        e.preventDefault();
+        e.stopPropagation();
+
+        this.props.onToggle(this.props.member);
+    };
+
+    _highlightName(str: string) {
+        if (!this.props.highlightWord) return str;
+
+        // We convert things to lowercase for index searching, but pull substrings from
+        // the submitted text to preserve case. Note: we don't need to htmlEntities the
+        // string because React will safely encode the text for us.
+        const lowerStr = str.toLowerCase();
+        const filterStr = this.props.highlightWord.toLowerCase();
+
+        const result = [];
+
+        let i = 0;
+        let ii;
+        while ((ii = lowerStr.indexOf(filterStr, i)) >= 0) {
+            // Push any text we missed (first bit/middle of text)
+            if (ii > i) {
+                // Push any text we aren't highlighting (middle of text match, or beginning of text)
+                result.push(<span key={i + 'begin'}>{str.substring(i, ii)}</span>);
+            }
+
+            i = ii; // copy over ii only if we have a match (to preserve i for end-of-text matching)
+
+            // Highlight the word the user entered
+            const substr = str.substring(i, filterStr.length + i);
+            result.push(<span className='mx_InviteDialog_roomTile_highlight' key={i + 'bold'}>{substr}</span>);
+            i += substr.length;
+        }
+
+        // Push any text we missed (end of text)
+        if (i < str.length) {
+            result.push(<span key={i + 'end'}>{str.substring(i)}</span>);
+        }
+
+        return result;
+    }
+
+    render() {
+        const client = MatrixClientPeg.get();
+        const member = this.props.member;
+        const avatarSize = 36;
+        const emailAvatar = require("../../../../res/img/icon-email-pill-avatar.svg");
+        const timestamp = this.props.lastActiveTs
+            ? <span className='mx_InviteDialog_roomTile_time'>{ humanizeTime(this.props.lastActiveTs) }</span>
+            : null;
+        const avatar = member.isEmail
+            ? <img
+                src={ emailAvatar } alt={_t("User Icon")}
+                width={ avatarSize } height={ avatarSize }/>
+            : <BaseAvatar
+                url={getHttpUriForMxc(
+                    client.getHomeserverUrl(), member.getMxcAvatarUrl(),
+                    avatarSize, avatarSize, "crop")}
+                name={ member.name }
+                idName={ member.userId }
+                width={ avatarSize }
+                height={ avatarSize } />;
+
+        let checkmark = null;
+        if (this.props.isSelected) {
+            // To reduce flickering we put the 'selected' room tile above the real avatar
+            checkmark = <div className='mx_InviteDialog_roomTile_selected' />;
+        }
+
+        // To reduce flickering we put the checkmark on top of the actual avatar (prevents
+        // the browser from reloading the image source when the avatar remounts).
+        const stackedAvatar = (
+            <span className='mx_InviteDialog_roomTile_avatarStack'>
+                { avatar }
+                { checkmark }
+            </span>
+        );
+
+        return (
+            <div className='mx_InviteDialog_roomTile' onClick={this._onClick}>
+                { stackedAvatar }
+                <span className='mx_InviteDialog_roomTile_name'>{this._highlightName(member.name)}</span>
+                <span className='mx_InviteDialog_roomTile_userId'>{this._highlightName(member.userId)}</span>
+                { timestamp }
+            </div>
+        );
+    }
+}
+
+export default class UserSelection extends React.Component {
+    static propTypes = {
+        onFinished: PropTypes.func,
+        roomId: PropTypes.string,
+    };
+
+    _debounceTimer: number = null;
+    _editorRef: any = null;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            targets: [], // array of Member objects (see interface above)
+            filterText: "",
+            suggestions: this._buildUserSuggestions(props.roomId),
+            numSuggestionsShown: INITIAL_USERS_SHOWN,
+            canUseIdentityServer: !!MatrixClientPeg.get().getIdentityServerUrl(),
+            tryingIdentityServer: false,
+            selectedRoom: "",
+        };
+
+        this._editorRef = createRef();
+    }
+
+    _updateUserSelectionViaCallback() {
+        this.props.userSelectionCallback(this.state.targets.map(u => u.userId));
+    }
+
+    _getRoomUsers(roomId: String): [] {
+        const client = MatrixClientPeg.get();
+        const room = client.getVisibleRooms().filter(room => room.roomId === roomId)[0];
+        return Object
+            .entries(room.currentState.members)
+            .flatMap(entry => {
+                return {userId: entry[0], user: entry[1]};
+            });
+    }
+
+    _selectAllUsers(roomId: string = RoomViewStore.getRoomId()) {
+        const allUsers = this._getRoomUsers(roomId);
+        const targets = [];
+        allUsers.forEach(u => {targets.push(u.user)});
+        return targets;
+    }
+
+    _buildUserSuggestions(roomId: string = RoomViewStore.getRoomId()): {userId: string, user: RoomMember}  {
+        return this._getRoomUsers(roomId);
+    }
+
+    _updateFilter = (e) => {
+        const term = e.target.value;
+        this.setState({filterText: term});
+    };
+
+    _showMoreSuggestions = () => {
+        this.setState({numSuggestionsShown: this.state.numSuggestionsShown + INCREMENT_USER_SHOWN});
+    };
+
+    _toggleMember = (member: Member) => {
+        const targets = this.state.targets.map(t => t); // cheap clone for mutation
+        const idx = targets.indexOf(member);
+        if (idx >= 0) {
+            targets.splice(idx, 1);
+        } else {
+            targets.push(member);
+            this.setState({filterText: ""}); // clear the filter when the user accepts a suggestion
+        }
+        this.setState({targets});
+        this._updateUserSelectionViaCallback();
+    };
+
+    _removeMember = (member: Member) => {
+        const targets = this.state.targets.map(t => t); // cheap clone for mutation
+        const idx = targets.indexOf(member);
+        if (idx >= 0) {
+            targets.splice(idx, 1);
+            this.setState({targets});
+        }
+        this._updateUserSelectionViaCallback();
+    };
+
+    _onClickInputArea = (e) => {
+        // Stop the browser from highlighting text
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (this._editorRef && this._editorRef.current) {
+            this._editorRef.current.focus();
+        }
+    };
+
+    _onUseDefaultIdentityServerClick = (e) => {
+        e.preventDefault();
+
+        // Update the IS in account data. Actually using it may trigger terms.
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        useDefaultIdentityServer();
+        this.setState({canUseIdentityServer: true, tryingIdentityServer: false});
+    };
+
+    _onManageSettingsClick = (e) => {
+        e.preventDefault();
+        dis.fire(Action.ViewUserSettings);
+    };
+
+    _renderUserList() {
+        let sourceMembers = this.state.suggestions;
+        let showNum = this.state.numSuggestionsShown;
+        const showMoreFn = this._showMoreSuggestions.bind(this);
+        const lastActive = (m) => null;
+
+        // Hide the section if there's nothing to filter by
+        if (sourceMembers.length === 0) return null;
+
+        // Do some simple filtering on the input before going much further. If we get no results, say so.
+        if (this.state.filterText) {
+            const filterBy = this.state.filterText.toLowerCase();
+            sourceMembers = sourceMembers
+                .filter(m => m.user.name.toLowerCase().includes(filterBy) || m.userId.toLowerCase().includes(filterBy));
+
+            if (sourceMembers.length === 0) {
+                return (
+                    <div className='mx_InviteDialog_section'>
+                        <p>{_t("No results")}</p>
+                    </div>
+                );
+            }
+        }
+
+        // If we're going to hide one member behind 'show more', just use up the space of the button
+        // with the member's tile instead.
+        if (showNum === sourceMembers.length - 1) showNum++;
+
+        const toRender = sourceMembers.slice(0, showNum);
+        const hasMore = toRender.length < sourceMembers.length;
+
+        const AccessibleButton = sdk.getComponent("elements.AccessibleButton");
+        const showMore = hasMore
+            ?   <AccessibleButton onClick={showMoreFn} kind="link">
+                    {_t("Show more")}
+                </AccessibleButton>
+            : null;
+
+        const tiles = toRender.map(r => (
+            <DMRoomTile
+                member={r.user}
+                lastActiveTs={lastActive(r)}
+                key={r.userId}
+                onToggle={this._toggleMember}
+                highlightWord={this.state.filterText}
+                isSelected={this.state.targets.some(t => t.userId === r.userId)}
+            />
+        ));
+        return (
+            <div className='mx_InviteDialog_section'>
+                { tiles }
+                { showMore }
+            </div>
+        );
+    }
+
+    _renderEditor() {
+        const input = (
+            <textarea
+                rows={1}
+                onChange={this._updateFilter}
+                value={this.state.filterText}
+                ref={this._editorRef}
+                autoFocus={true}
+            />
+        );
+        return (
+            <div className='mx_InviteDialog_editor' onClick={this._onClickInputArea}>
+                {input}
+            </div>
+        );
+    }
+
+    _renderIdentityServerWarning() {
+        if (!this.state.tryingIdentityServer || this.state.canUseIdentityServer) {
+            return null;
+        }
+
+        const defaultIdentityServerUrl = getDefaultIdentityServerUrl();
+        if (defaultIdentityServerUrl) {
+            return (
+                <div className="mx_AddressPickerDialog_identityServer">{_t(
+                    "Use an identity server to invite by email. " +
+                    "<default>Use the default (%(defaultIdentityServerName)s)</default> " +
+                    "or manage in <settings>Settings</settings>.",
+                    {
+                        defaultIdentityServerName: abbreviateUrl(defaultIdentityServerUrl),
+                    },
+                    {
+                        default: sub => <a href="#" onClick={this._onUseDefaultIdentityServerClick}>{sub}</a>,
+                        settings: sub => <a href="#" onClick={this._onManageSettingsClick}>{sub}</a>,
+                    },
+                )}</div>
+            );
+        } else {
+            return (
+                <div className="mx_AddressPickerDialog_identityServer">{_t(
+                    "Use an identity server to invite by email. " +
+                    "Manage in <settings>Settings</settings>.",
+                    {}, {
+                        settings: sub => <a href="#" onClick={this._onManageSettingsClick}>{sub}</a>,
+                    },
+                )}</div>
+            );
+        }
+    }
+
+    render() {
+
+        // only do this, if the parentRoom changed
+        if(this.props.roomId !== this.state.selectedRoom) {
+            // generate list of all users in the selected room
+            this.state.suggestions = this._buildUserSuggestions(this.props.roomId);
+            // pre-select all users
+            this.state.targets = this._selectAllUsers(this.props.roomId);
+            // update the roomId in state to track when the parentRoom changes
+            this.state.selectedRoom = this.props.roomId;
+        }
+
+        const userId = MatrixClientPeg.get().getUserId();
+        const helpText = _t(
+            "Invite someone using their name, email address, or username (like <userId/>).",
+            {},
+            {
+                userId: () => <a href={makeUserPermalink(userId)} rel="noreferrer noopener" target="_blank">{userId}</a>,
+            },
+        );
+
+        return (
+            <div className='mx_InviteDialog_content'>
+                <p className='mx_InviteDialog_helpText'>{helpText}</p>
+                <div className='mx_InviteDialog_addressBar'>
+                    {this._renderEditor()}
+                </div>
+                {this._renderIdentityServerWarning()}
+                <div className='mx_InviteDialog_userSections'>
+                    {this._renderUserList()}
+                </div>
+            </div>
+        );
+    }
+}
